7b30735f8a50422f1151f9a597171fab610592c0
Matt Glaman <nmd.matt@gmail.com>
2021-05-14 14:40:20 -0500

    Add CORS and sample code to frontend

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,29 @@
+import { useState, useEffect } from 'react'
 import logo from './logo.svg';
 import './App.css';
+
+function JsonApiEntrypoint() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}jsonapi`)
+      .then(res => res.json())
+      .then(json => setData(json));
+  }, [])
+
+  if (data === null) {
+    return null;
+  }
+
+  return <code><pre>{JSON.stringify(data, null, 4)}</pre></code>
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <JsonApiEntrypoint />
     </div>
   );
 }
